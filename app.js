c9077caf05f497b9259ca44542c23a6199240dfa
Jhonatan Ostroski <jhonatan-bruno@hotmail.com>
2023-07-02 15:23:21 -0300

    Pokedex

---
+++ app.js
@@ -0,0 +1,111 @@
+const getTypeColor = type => {
+  const normal = '#F5F5F5'
+  return {
+    normal,
+    fire: '#FDDFDF',
+    grass: '#DEFDE0',
+    electric: '#FCF7DE',
+    ice: '#DEF3FD',
+    water: '#DEF3FD',
+    ground: '#F4E7DA',
+    rock: '#D5D5D4',
+    fairy: '#FCEAFF',
+    poison: '#98D7A5',
+    bug: '#F8D5A3',
+    ghost: '#CAC0F7',
+    dragon: '#97B3E6',
+    psychic: '#EAEDA1',
+    fighting: '#E6E0D4'
+  }[type] || normal
+}
+
+//retorna apenas pokemons válidos
+const getOnlyFulfilled = async ({arr, func}) => {
+  const promises = arr.map(func)
+  const responses = await Promise.allSettled(promises)
+  return responses.filter(response => response.status === 'fulfilled')
+}
+
+//retorna o tipo do pokemon
+const getPokemonsType = async pokeApiResults => {
+  const fulfilled = await getOnlyFulfilled({arr: pokeApiResults, func: result => fetch(result.url)})
+  const Pokepromises = fulfilled.map(url => url.value.json())
+  const pokemons = await Promise.all(Pokepromises)
+  return pokemons.map(fulfilled => fulfilled.types.map(info => info.type.name))
+}
+
+//retorna os IDs dos pokemons
+const getPokemonsIds = pokeApiResults => pokeApiResults.map(({ url}) => {
+  const urlAsArray = url.split('/')
+  return urlAsArray.at(urlAsArray.length - 2)
+})
+
+//retorna as imagens dos pokemons, baseados nos IDs
+const getPokemonsImg = async ids => {
+  const fulfilled = await getOnlyFulfilled({arr: ids, func: id => fetch (`./assets/img/${id}.png`) })
+  return fulfilled.map(response => response.value.url)
+}
+
+
+const getPokemons = async () => {
+  try {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=15&offset=0')
+    if (!response.ok) {
+      throw new Error ('Não foi possivel obter as informações!')
+    }
+    const {results: pokeApiResults} = await response.json()
+    const types = await getPokemonsType(pokeApiResults)
+    const ids = getPokemonsIds(pokeApiResults)
+    const imgs = await getPokemonsImg(ids)
+    const pokemons = ids.map((id, i) => ({id:id, name: pokeApiResults[i].name, types: types[i], imgUrl: imgs[i] }))
+    return pokemons
+  } 
+catch (error) {
+  console.log('Algo deu errado!', error.message)
+}
+}
+
+const renderPokemons = pokemons => {
+  const ul = document.querySelector('[data-js="pokemons-list"]')
+
+  const fragment = document.createDocumentFragment()
+
+  pokemons.forEach(({id, name, types, imgUrl}) => {
+    const li = document.createElement('li')
+    const img = document.createElement('img')
+    const nameContainer = document.createElement('h2')
+    const typeContainer = document.createElement('p')
+    const [firstType] = types
+
+    img.setAttribute('src', imgUrl)
+    img.setAttribute('alt', name)
+    img.setAttribute('class', 'card-image')
+    li.setAttribute('class', `card ${firstType}`)
+    li.style.setProperty('--type-color', getTypeColor(firstType))
+
+    nameContainer.textContent = `${id}. ${name[0].toUpperCase()}${name.slice(1)}`
+    typeContainer.textContent = types.length > 1 ? types.join(' | ') : firstType
+    li.append(img, nameContainer, typeContainer)
+
+    fragment.append(li)
+  })
+  ul.append(fragment)
+}
+
+
+
+
+
+
+const paginaCarregada = async () => {
+  const pokemons = await getPokemons()
+  //console.log(pokemons)
+  renderPokemons(pokemons)
+
+
+
+
+} 
+
+
+paginaCarregada()
